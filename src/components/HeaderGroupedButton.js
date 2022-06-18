7e421c7439be94a2ad4006a0c1482af0ac5e00a8
Itse Sagay <codeplay.erniie@gmail.com>
2022-06-18 04:14:46 +0100

    added fontawesome react library to project

--- src/components/HeaderGroupedButton.js
+++ src/components/HeaderGroupedButton.js
@@ -1,10 +1,13 @@
 import '../styles/headergroupedbutton.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function HeaderGroupedButton({ title }) {
   return (
     <div className="header__grouped-btn">
         <a href="/" className="btn">{title}</a>
-        <button className="dropdown"><ion-icon name="chevron-down-outline"></ion-icon></button>
+      <button className="dropdown">
+        <FontAwesomeIcon icon="fa-solid fa-angle-down" />
+      </button>
     </div>
   )
 }
